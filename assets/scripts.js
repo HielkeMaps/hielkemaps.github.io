7b11f5284112cfc6215424bfce183ae8429289b7
Hielke <62292398+HielkeMaps@users.noreply.github.com>
2023-04-28 22:05:38 +0200

    Update scripts.js

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,16 +1,30 @@
-$(function() {
+$(function () {
 	$('[data-toggle="tooltip"]').tooltip()
+
+	//Remove commento text
+	const observer = new MutationObserver(function(mutationsList) {
+		for (let mutation of mutationsList) {
+		  if (mutation.type === 'childList') {
+			const element = document.querySelector(".commento-logo-text");
+			if (element) {
+			  element.remove();
+			  observer.disconnect();
+			}
+		  }
+		}
+	  });
+	  
+	  observer.observe(document.body, { childList: true, subtree: true });	  
 })
 
-$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+$(document).on('click', '[data-toggle="lightbox"]', function (event) {
 	event.preventDefault();
 	$(this).ekkoLightbox();
 });
 
 //bootstrap hack to make sure you can't scroll the page when any modal is open
 $(document).on('hidden.bs.modal', function () {
-	if($('.modal.show').length)
-	{
-	  $('body').addClass('modal-open');
+	if ($('.modal.show').length) {
+		$('body').addClass('modal-open');
 	}
-  });
+});
