12ccc33828dc6c87363f841ab37d110738b57e09
Hielke <62292398+HielkeMaps@users.noreply.github.com>
2023-04-29 13:28:42 +0200

    Redesigned lightbox

--- assets/scripts.js
+++ assets/scripts.js
@@ -2,19 +2,19 @@ $(function () {
 	$('[data-toggle="tooltip"]').tooltip()
 
 	//Remove commento text
-	const observer = new MutationObserver(function(mutationsList) {
+	const observer = new MutationObserver(function (mutationsList) {
 		for (let mutation of mutationsList) {
-		  if (mutation.type === 'childList') {
-			const element = document.querySelector(".commento-logo-text");
-			if (element) {
-			  element.remove();
-			  observer.disconnect();
+			if (mutation.type === 'childList') {
+				const element = document.querySelector(".commento-logo-text");
+				if (element) {
+					element.remove();
+					observer.disconnect();
+				}
 			}
-		  }
 		}
-	  });
-	  
-	  observer.observe(document.body, { childList: true, subtree: true });	  
+	});
+
+	observer.observe(document.body, { childList: true, subtree: true });
 })
 
 $(document).on('click', '[data-toggle="lightbox"]', function (event) {
@@ -27,4 +27,26 @@ $(document).on('hidden.bs.modal', function () {
 	if ($('.modal.show').length) {
 		$('body').addClass('modal-open');
 	}
+});
+
+Fancybox.bind("[data-fancybox]", {
+	wheel: "slide",
+	Thumbs: {
+		type: "classic",
+	},
+	Toolbar: {
+		display: {
+		  left: ['close'],
+		  middle: [],
+		  right: [],
+		},
+	  },
+	Images: {
+		initialSize: "fit",
+	},
+
+	Carousel: {
+		transition: "classic",
+		friction: 0,
+	  }
 });
