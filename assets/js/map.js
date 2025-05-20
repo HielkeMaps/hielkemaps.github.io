a3c4b31e9363638515821434655b19fa3f4ab89b
Hielke <hielkemaps@gmail.com>
2025-05-20 19:30:04 +0200

    Use my own email server

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,25 +1,28 @@
-const downloadLink = document.querySelector(".java-download-btn");
-
-downloadLink.addEventListener("click", function (event) {
-  $("#downloadModal").modal("hide");
-
-  if (this.href) {
-    window.location.href = this.href; //Open download link. We need to do this again because the modal toggle overrides it by default.
-  }
-});
-
 $(function () {
-  const observer = new MutationObserver(function (mutationsList) {
-    for (let mutation of mutationsList) {
-      if (mutation.type === "childList") {
-        const element = document.querySelector("[eo-block='powered-by']");
-        if (element) {
-          element.remove();
-          observer.disconnect();
+  // Handle all download buttons
+  const downloadLinks = document.querySelectorAll(".java-download-btn");
+  
+  downloadLinks.forEach(link => {
+    // Only add click handler to links without modal toggle
+    if (!link.hasAttribute('data-bs-toggle')) {
+      link.addEventListener("click", function (event) {
+        $("#downloadModal").modal("hide");
+        
+        if (this.href) {
+          window.location.href = this.href; //Open download link. We need to do this again because the modal toggle overrides it by default.
         }
-      }
+      });
     }
   });
-
-  observer.observe(document.body, { childList: true, subtree: true });
+  
+  // Handle modal download button
+  const modalDownloadBtn = document.querySelector('.java-download-btn[data-bs-toggle="modal"]');
+  if (modalDownloadBtn) {
+    const downloadUrl = modalDownloadBtn.getAttribute('href');
+    
+    // When the modal is shown, trigger the download
+    $('#thanksDownloadModal').on('shown.bs.modal', function () {
+      window.location.href = downloadUrl;
+    });
+  }
 });
---
+++ assets/js/newsletter.js
@@ -0,0 +1,45 @@
+$(function () {
+    // Handle newsletter form submission
+    const newsletterForm = document.querySelector('#newsletter-form');
+    
+    if (newsletterForm) {
+        newsletterForm.addEventListener('submit', function(event) {
+            event.preventDefault(); // Prevent the default form submission
+            
+            const emailInput = this.querySelector('input[name="email"]');
+            const hpInput = this.querySelector('input[name="hp"]');
+            const listInput = this.querySelector('input[name="list"]');
+            const subformInput = this.querySelector('input[name="subform"]');
+            
+            const formData = new FormData();
+            formData.append('email', emailInput.value);
+            formData.append('hp', hpInput.value); // This should be empty (honeypot)
+            formData.append('list', listInput.value);
+            formData.append('subform', subformInput.value);
+            
+            // Show loading state
+            const submitBtn = this.querySelector('input[type="submit"]');
+            const originalBtnText = submitBtn.value;
+            submitBtn.value = 'Subscribing...';
+            submitBtn.disabled = true;
+            
+            // Send the form data using fetch
+            fetch('https://newsletter.hielkemaps.com/subscribe', {
+                method: 'POST',
+                body: formData
+            })
+            .then(response => {
+                if (response.ok) {
+                    newsletterForm.innerHTML = '<div class="text-center"><img src="/media/home/yay.gif" alt="Yay!" style="border-radius: 50%; width: 150px;"><p>Thanks for subscribing!</p></div>'
+                }
+            })
+            .catch(error => {
+                submitBtn.value = 'Try again later...';
+                setTimeout(() => {
+                    submitBtn.value = originalBtnText;
+                    submitBtn.disabled = false;
+                }, 3000);
+            });
+        });
+    }
+});
